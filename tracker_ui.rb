58ad51ae197e576794ff26cd497b784376c6896d
Nick Bernard & JJ Idt <student@epicodus.com>
2014-08-11 11:58:27 -0700

    Add functionality to create and list divisions

--- tracker_ui.rb
+++ tracker_ui.rb
@@ -1,6 +1,7 @@
 require 'active_record'
 require 'pry'
 require './lib/employee'
+require './lib/division'
 
 database_configurations = YAML::load(File.open('./db/config.yml'))
 development_configuration = database_configurations['development']
@@ -12,17 +13,32 @@ def welcome
   menu
 end
 
+def ws
+  puts "\n"
+end
+
 def menu
-  puts "A > Add an employee"
-  puts "L > List employees"
-  puts "S > Search for employee"
+  puts "AE > Add an employee"
+  puts "LE > List employees"
+  ws
+  puts "AD > Add a division"
+  puts "LD > List divisions"
+  puts "ED > Assign employee to a division"
+  ws
+  puts "S > Search for an employee"
   puts "E > Exit"
 
     case gets.chomp.upcase
-    when 'A'
+    when 'AE'
       add_employee
-    when 'L'
+    when 'LE'
       list_employees
+    when 'AD'
+      add_division
+    when 'LD'
+      list_divisions
+    when 'ED'
+      assign_division
     when 'S'
       search
     when 'E'
@@ -40,7 +56,7 @@ def add_employee
 end
 
 def list_employees
-  puts "Employees"
+  puts "Employees:"
   Employee.all.each {|employee| puts "#{employee.name}"}
   menu
 end
@@ -48,6 +64,19 @@ end
 def search
   puts "Enter the employee's name you want to search for:"
   test = Employee.where(:name => gets.chomp).all.each {|employee| puts "#{employee.name}"}
+  menu
+end
+
+def add_division
+  puts "Enter the name of a new division:"
+  new_division = Division.create(:name => gets.chomp)
+  puts "'#{new_division.name}' created!"
+  menu
+end
+
+def list_divisions
+  puts "Divisions:"
+  Division.all.each {|division| puts "#{division.name}"}
   menu
 end
 
